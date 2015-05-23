f408634d1b97fe5c82270c7bc6010fbca2594712
DILYS <optanovo@gmail.com>
2015-05-22 20:00:50 -0700

    Initial Commit

---
+++ RSB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSB
+//
+//  Created by Yu Sun on 5/22/15.
+//  Copyright (c) 2015 Yu Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
